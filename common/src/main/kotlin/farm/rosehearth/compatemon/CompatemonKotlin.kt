694eab8185c47c2ea00a576021701ecfb158d474
RosehearthFarms-dev <amos@rosehearth.farm>
2024-01-24 13:35:06 -0500

    Got it adding the scale to the entity files at least! Next step is making it load to the pokemon and then also to the party/box data

--- common/src/main/kotlin/farm/rosehearth/compatemon/CompatemonKotlin.kt
+++ common/src/main/kotlin/farm/rosehearth/compatemon/CompatemonKotlin.kt
@@ -1,32 +1,38 @@
 package farm.rosehearth.compatemon
 
+
+
+import com.cobblemon.mod.common.api.events.CobblemonEvents.POKEMON_ENTITY_SAVE
 import com.cobblemon.mod.common.api.properties.CustomPokemonProperty
-import com.cobblemon.mod.common.util.splitMap
 import farm.rosehearth.compatemon.api.cobblemon.properties.SizeScaleProperty
+import net.minecraft.nbt.CompoundTag
 import org.apache.logging.log4j.LogManager
+import virtuoel.pehkui.api.ScaleTypes
+import virtuoel.pehkui.util.ScaleUtils
 
 object CompatemonKotlin {
 
     fun initialize() {
         val LOGGER = LogManager.getLogger()
-
-
-        var s = "\"1.25\""
-
-        var x = s.splitMap(" ", "=")
         LOGGER.info("----------------------------------------------------------------------------------")
-        LOGGER.info("----------------------------------------------------------------------------------")
-        LOGGER.info("----------------------------------------------------------------------------------")
-        LOGGER.info("----------------------------------------------------------------------------------")
-        x.forEach { LOGGER.info(it.first + " ::: " + it.second) }
-        LOGGER.info("----------------------------------------------------------------------------------")
-        LOGGER.info("----------------------------------------------------------------------------------")
-        LOGGER.info("----------------------------------------------------------------------------------")
+        LOGGER.info("Initializing Compatemon")
         LOGGER.info("----------------------------------------------------------------------------------")
 
         CustomPokemonProperty.register(SizeScaleProperty)
+        POKEMON_ENTITY_SAVE.subscribe{ event ->
+            var baseScale = ScaleUtils.getTypedScale(event.pokemonEntity,ScaleTypes.BASE,1.0f)
+            var compatScaleNBT = CompoundTag()
+            compatScaleNBT.putFloat("peh_sizeScale", baseScale)
+            compatScaleNBT.putFloat("peh_weightScale", baseScale)
+            var compatNBT = CompoundTag()
+            compatNBT.put("Compatemon",compatScaleNBT)
 
-        CustomPokemonProperty.properties.forEach { LOGGER.info(it.fromString(s)?.asString()) }
+            event.nbt.put("Pokemon",compatNBT)
+
+        }
+       // POKEMON_CAPTURED.subscribe {event ->
+       //
+       // }
     }
 
 
