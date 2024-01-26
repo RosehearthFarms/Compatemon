7442e7bfe3c8209e0747beba3a9b3eaafb0d7ad4
RosehearthFarms-dev <amos@rosehearth.farm>
2024-01-26 15:24:11 -0500

    Updated to 0.0.2. Cleaned up code and files and stuff. Fabric may be working, haven't tested. Forge IS working.

--- fabric/src/main/java/farm/rosehearth/compatemon/CompatemonFabric.java
+++ fabric/src/main/java/farm/rosehearth/compatemon/CompatemonFabric.java
@@ -1,6 +1,5 @@
 package farm.rosehearth.compatemon;
 
-import farm.rosehearth.compatemon.event.EntityLoadHandler;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 
@@ -9,10 +8,7 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 public class CompatemonFabric implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		Compatemon.LOGGER.info("Hello Fabric world from Fight or Flight!");
-
-		//CobblemonSizes.init((pokemonEntity, priority, goal) -> ((MobEntityAccessor) (Object) pokemonEntity).goalSelector().addGoal(priority, goal));
-
-		ServerEntityEvents.ENTITY_LOAD.register(new EntityLoadHandler());
+		Compatemon.LOGGER.info("Hello Fabric world from Compatemon!");
+		Compatemon.init();
 	}
 }
--- fabric/src/main/java/farm/rosehearth/compatemon/mixin/MobEntityAccessor.java
+++
@@ -1,9 +0,0 @@
-package farm.rosehearth.compatemon.mixin;
-
-import net.minecraft.world.entity.Mob;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(Mob.class)
-public interface MobEntityAccessor {
-
-}
